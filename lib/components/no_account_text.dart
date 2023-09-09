126e4f37a89c95c1ae703692deffc606b6a93b76
Duy Duc Nguyen <duyduck2809@gmail.com>
2023-09-09 17:55:47 +0700

    complete sign in + forgot pass + login success

---
+++ lib/components/no_account_text.dart
@@ -0,0 +1,32 @@
+import 'package:e_commerce_app/constants.dart';
+import 'package:e_commerce_app/screens/forgot_password/forgot_password_screen.dart';
+import 'package:e_commerce_app/size_config.dart';
+import 'package:flutter/material.dart';
+
+class NoAccountText extends StatelessWidget {
+  const NoAccountText({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "Don’t have an account? ",
+          style: TextStyle(fontSize: getProportionateScreenWidth(16)),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Text(
+            "Sign Up",
+            style: TextStyle(
+                fontSize: getProportionateScreenWidth(16),
+                color: kPrimaryColor),
+          ),
+        ),
+      ],
+    );
+  }
+}
